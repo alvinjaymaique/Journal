eac3a8e06fc19db5ed85984cf9641ebfad130352
alvinjaymaique <alvinjay.maique.56@gmail.com>
2025-01-24 12:12:14 +0800

    First commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <div class=''>
+        <h1><Header></Header></h1>
+      </div> */}
+      {/* <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1> */}
+    </>
+  )
+}
+
+export default App
